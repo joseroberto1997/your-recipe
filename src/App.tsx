5c92bb9eb8a35b4d628ebc29aa5062439554a5ad
jose.neto <jose.neto@infracommerce.com.br>
2024-04-15 22:26:39 -0300

    feat: create sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { SideBarMenu } from "./components/SidebarMenu";
 
 function App() {
-  return <></>;
+  return (
+    <div className="container">
+      <SideBarMenu />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/SidebarMenu/index.tsx
@@ -0,0 +1,9 @@
+import "./style.css";
+
+export const SideBarMenu = () => {
+  return (
+    <div className="sidebar">
+      <span>Your Recipe</span>
+    </div>
+  );
+};
