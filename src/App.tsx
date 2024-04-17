1a2ac59d09cc21318eeacd73bdea257a37b89a49
jose.neto <jose.neto@infracommerce.com.br>
2024-04-16 21:17:43 -0300

    feat: add route and style sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { SideBarMenu } from "./components/SidebarMenu";
+import { MainContent } from "./components/MainContent";
 
 function App() {
   return (
     <div className="container">
       <SideBarMenu />
+      <main>
+        <Routes>
+          <Route path="/:categoryId" element={<MainContent />} />
+        </Routes>
+      </main>
     </div>
   );
 }
--- src/components/SidebarMenu/index.tsx
+++ src/components/SidebarMenu/index.tsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
 import "./style.css";
 
+interface CategoryProps {
+  strCategory: string;
+}
+
 export const SideBarMenu = () => {
+  const [categoriesMenu, setCategoriesMenu] = useState([]);
+
+  useEffect(() => {
+    const fetchMenuItens = async () => {
+      try {
+        const response = await axios.get(
+          "https://www.themealdb.com/api/json/v1/1/categories.php"
+        );
+        setCategoriesMenu(response.data?.categories);
+      } catch (error) {
+        console.log("Erro ao buscar dados na api", error);
+      }
+    };
+
+    fetchMenuItens();
+  }, []);
   return (
     <div className="sidebar">
       <span>Your Recipe</span>
+      <ul>
+        {categoriesMenu.map((category: CategoryProps) => (
+          <li>
+            <Link to={`/${category?.strCategory}`}>
+              {category?.strCategory}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
